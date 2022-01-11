efb594c49e95449031984867c3645217c317dbf0
Mohsen Pourjam <m.pourjam@tum.de>
2022-01-11 17:13:54 +0100

    task_pickle class added.

---
+++ getENA_modified.py
@@ -0,0 +1,149 @@
+#! /usr/bin/env python
+import argparse
+from sys import version_info
+import pandas as pd
+import pickle as pk
+from multiprocessing.pool import ThreadPool
+from tqdm import tqdm  # pip3 install tqdm
+from hashlib import md5
+from itertools import repeat
+from os import getcwd, cpu_count
+from datetime import datetime
+# from mimetypes import guess_type
+# from datetime import datetime as dt
+from task_classes import task_pickle
+if version_info[0] < 3:
+    from StringIO import StringIO
+    from pathlib2 import Path  # pip2 install pathlib2
+else:
+    from io import StringIO
+    from pathlib import Path
+try:
+    from urllib.request import urlretrieve
+    from urllib.request import urlopen
+    from urllib.error import (ContentTooShortError, URLError)
+except ImportError:
+    from urllib import urlretrieve
+    from urllib import urlopen
+    from urllib.error import (ContentTooShortError, URLError)
+
+
+global tsvurl
+tsvurl = 'https://www.ebi.ac.uk/ena/portal/api/filereport?accession='
+tsvurl += '{}&fields=all&result=read_run'
+status_dict = {"Done": 0, "Progress": 1, "Error": -1}
+
+
+def md5sum(filename, blocksize=65536):
+    hash = md5()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(blocksize), b''):
+            hash.update(block)
+    return hash.hexdigest()
+
+
+def download_fastq(inputdata):
+    df, outpath = inputdata
+    index, row = df
+    codename = row.name
+    md5cheked = False
+    while not md5cheked:
+        listmd5 = []
+        for pair, md5pair in zip(row['fastq_ftp'].split(';'),
+                                 row['fastq_md5'].split(';')):
+            # TODO replace the experiment_accession with tax_id if applicable
+            outfile = outpath.joinpath(row['study_accession'])
+            outfile = outfile.joinpath(row['experiment_accession'])
+            outfile = outfile.joinpath(row['sample_accession'])
+            outfile = outfile.joinpath(row['run_accession'])
+            outfile.mkdir(exist_ok=True, parents=True)
+            # Creating the Args dictionary an storing it as pickle
+            paired = "Yes" if row["library_layout"] == "PAIRED" else "No"
+            pickle_file_path = outfile.joinpath("{}.pk".format(row['run_accession']))
+            pk_obj = task_pickle(pickle_file_path)
+            pk_obj.task_dict["args"]["input_dir"] = str(outfile.resolve())
+            pk_obj.task_dict["args"]["paired"] = paired
+            pk_obj.task_dict["args"]["input_id"] = str(row['run_accession'])
+            pk_obj.task_dict["status"]["download"] = pk_obj.scode_d["Started"]
+            pk_obj.write()
+            outfile = outfile.joinpath(pair.split('/')[-1])
+            if outfile.is_file() and md5sum(outfile) == md5pair:
+                listmd5.append(1)
+            else:
+                try:
+                    pk_obj.task_dict["status"]["download"] = pk_obj.scode_d["Progress"]
+                    pk_obj.write()
+                    urlretrieve('ftp://' + pair, outfile)
+                except Exception as e:
+                    listmd5.append(0)
+                    break
+                if md5sum(outfile) == md5pair:
+                    listmd5.append(1)
+                else:
+                    listmd5.append(0)
+        if all(listmd5):
+            pk_obj.task_dict["status"]["download"] = pk_obj.scode_d["Done"]
+            pk_obj.write()
+            md5cheked = True
+    return '[OK] {}'.format(codename)
+
+
+def urlretrieve_converter(url_path, attmp=0):
+    if attmp > 15:
+        print('Error', *url_path)
+        return False
+    try:
+        return urlretrieve(*url_path)
+    except ContentTooShortError as e:
+        print('Retry {}/15'.format(attmp + 1), *url_path)
+        urlretrieve_converter(url_path, attmp + 1)
+    except URLError as e:
+        print('Retry {}/15'.format(attmp + 1), *url_path)
+        urlretrieve_converter(url_path, attmp + 1)
+
+
+def main(acc, outdir=None, threads=None):
+    if not isinstance(acc, list):
+        exit("acc argument must be a list of accession numbers")
+    outputpath = Path(outdir) if outdir else Path(getcwd())
+    tmpoutputpath = outputpath/'tmp'
+    tmpoutputpath.mkdir(exist_ok=True, parents=True)
+    accurl = [tsvurl.format(accid) for accid in acc]
+    accout = [tmpoutputpath/(accid+'.tsv') for accid in acc]
+    metadata = []
+    multipleargs = [(u, a) for (u, a) in zip(accurl, accout) if not a.is_file()]
+    with ThreadPool(args.threads) as p:
+        for result in tqdm(p.imap_unordered(urlretrieve_converter, multipleargs),
+                           total=len(multipleargs),
+                           desc='Downloading metadatas using {} threads'.format(args.threads),
+                           unit='metadatas'):
+            metadata.append(result)
+    frames = [pd.read_csv(tsv, sep='\t') for tsv in accout]  # , index_col=5
+    concat_frames = pd.concat(frames, ignore_index=True)
+    genome = []
+    with ThreadPool(args.threads) as p:
+        multipleargs = list(zip(concat_frames.iterrows(), repeat(outputpath)))
+        for result in tqdm(p.imap_unordered(download_fastq, multipleargs),
+                           total=len(multipleargs),
+                           desc='Downloading Genomes using {} threads'.format(args.threads),
+                           unit='Genomes'):
+            genome.append(result)
+    concat_frames.to_csv(tmpoutputpath/'metadata_{}.tsv'.format(datetime.today().strftime('%Y%m%d')),
+                         index=False,
+                         sep='\t')
+
+
+if __name__ == '__main__':
+    V = '%(prog)s v1.2.5'
+    parser = argparse.ArgumentParser(description='Download FASTQ files from ENA ({})'.format(V))
+    parser.add_argument('-acc', '--acc', nargs='+')
+    parser.add_argument('-o', '--outfiles', type=str,
+                        default=getcwd() + '/' + 'ENA_out')
+    parser.add_argument('-t', '--threads', type=int,
+                        default=int(cpu_count() * 0.5),
+                        help='Number of threads to use (default 12)')
+    parser.add_argument('-V', '--version', action='version',
+                        version=V)
+    args = parser.parse_args()
+    args.outfiles = Path(args.outfiles)/"ENA_out"
+    main([args.acc], args.outfiles, args.threads)
---
+++ task_classes.py
@@ -0,0 +1,122 @@
+import pickle as pk
+from os import getcwd
+from pathlib import Path
+from mimetypes import guess_type
+from datetime import datetime as dt
+from collections import OrderedDict as OD
+
+
+class task_pickle:
+    path = getcwd()
+    scode_d = {"Done": 0,
+               "Queue": 1,
+               "Started": 2,
+               "Progress": 3,
+               "Error": -1
+               }
+    args_dict = OD({"input_dir": "",
+                    "paired": "",
+                    "forward_file": "",
+                    "reverse_file": "",
+                    "input_id": "",
+                    "spike_amount": 6
+                    })
+    status_dict = {"download": scode_d["Queue"], "run": scode_d["Queue"]}
+    task_dict = {"args": args_dict, "status": status_dict}
+
+    def check_args_dict(self, args_dict):
+        if not isinstance(args_dict, dict):
+            return False
+        keys = ["input_dir",
+                "paired",
+                "forward_file",
+                "reverse_file",
+                "input_id",
+                "spike_amount"]
+        has_key_list = [True for k in keys if k not in args_dict]
+        if any(has_key_list):
+            return False
+        return True
+
+    def check_status_dict(self, status_dict):
+        if not isinstance(status_dict, dict):
+            return False
+        keys = ["download", "run"]
+        has_key_list = [True for k in keys if k not in status_dict]
+        if any(has_key_list):
+            return False
+        return True
+
+    def set_task_dict(self, task_dict):
+        if isinstance(task_dict, dict):
+            keys = ["args", "status"]
+            if any([True for k in keys if k not in task_dict]):
+                msg = "Task dict must have dictionaries 'args' and 'status'."
+                raise TypeError(msg)
+            if not self.check_args_dict(task_dict["args"]):
+                msg = "Argument dictionary is not formatted correclty."
+                raise TypeError(msg)
+            if not self.check_status_dict(task_dict["status"]):
+                msg = "Status dictionary is not formatted correclty."
+                raise TypeError(msg)
+        else:
+            msg = "Given object as task dictionary is not a dictionary."
+            raise TypeError()
+        self.task_dict = task_dict
+        return True
+
+    def __init__(self, filepath=None, task_dict=None):
+        timestamp = dt.now().strftime('%Y%m%d_%H%M%S')
+        path = Path(filepath) if filepath else Path(self.path)
+        if path.is_dir():
+            path = path.joinpath("{}_proc_task_d.pk".format(str(timestamp)))
+        self.path = path.resolve()
+
+        if self.path.exists():
+            f_type, f_enc = guess_type(str(self.path))
+            if "x-tex-pk" not in f_type:
+                raise FileExistsError("File is not a pickle file.")
+            with open(self.path, "rb") as pkfile:
+                file_content = pk.load(pkfile)
+            self.set_task_dict(file_content)
+
+        if task_dict:
+            self.set_task_dict(task_dict)
+
+    def write(self):
+        with open(self.path, 'wb') as pkfile:
+            pk.dump(self.task_dict, pkfile, protocol=pk.HIGHEST_PROTOCOL)
+
+    def args_complete(self):
+        arg_d = self.task_dict["args"]
+        input_d = arg_d["input_d"]
+        for k, v in arg_d.items():
+            if k == "input_d":
+                try:
+                    p = Path(v)
+                except Exception as e:
+                    return False
+                if not p.exists():
+                    return False
+            elif k == "forward_file":
+                p = Path(input_d)/str(v)
+                if not p.exists():
+                    return False
+            elif k == "reverse_file":
+                p = Path(input_d)/str(v)
+                if not p.exists():
+                    return False
+            elif k == "paired":
+                if v != "Yes" or v != "No":
+                    return False
+            elif k == "input_id":
+                if not v:
+                    return False
+            elif k == "spike_amount":
+                try:
+                    v = int(v)
+                except Exception as e:
+                    return False
+            else:  # Existence of any other arguments
+                return False
+        return True
