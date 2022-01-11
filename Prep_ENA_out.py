81f7c652490f5d822a622a41e7eb30b5f1d9feca
Mohsen Pourjam <m.pourjam@tum.de>
2022-01-11 17:13:54 +0100

    task_pickle class added.

--- Prep_ENA_out.py
+++ Prep_ENA_out.py
@@ -2,10 +2,9 @@
 import os
 import re
 import argparse
-from pprint import pprint
+from pprint import pprint as print
 import datetime
 import mimetypes
-import shutil
 from random import randint
 import pickle as pk
 from collections import OrderedDict as OD
@@ -14,15 +13,20 @@ from pathlib import Path
 from statistics import mean, stdev
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
+from task_classes import task_pickle
 
 max_pool = int(cpu_count() * 0.4)
 pool_size = max_pool if max_pool > 0 else 1
-forw_file_match = re.compile("(_R1_|_1\.|_F)")
+forw_file_match = re.compile(r"(_R1_|_1\.|_F)")
+
 
 def proper_length(forw_fastqfile):
-    # import re
-    # seq_match = re.compile(r'[^ACTGN]')
-    to_assess = [randint(1,10000) for i in range(1, 5000)]
+    n_first_lines = 10000
+    assess_coeff = 0.5
+    min_len = 150
+    max_len_std = 1
+    sample_range = round(assess_coeff * n_first_lines)
+    to_assess = [randint(1, n_first_lines) for i in range(1, sample_range)]
     file_path = Path(forw_fastqfile)
     seq_counter = 0
     random_length = []
@@ -31,40 +35,49 @@ def proper_length(forw_fastqfile):
         if not line.startswith("@"):
             raise TypeError("Faulty fastq file: {}".format(file_path))
 
-        while line and seq_counter < 10000:
-            #NOTE This is only to get quality line or seq line and assess the length
-            #NOTE (even some quality lines might be discarded if they start with @ or + signs.)
+        while line and seq_counter < n_first_lines:
+            # NOTE This is only to get quality line or seq line and assess the length
+            # NOTE (even some quality lines might be discarded if they start with @ or + signs.)
             if line.startswith("@") or line.startswith("+"):
-                # prviously_matched = False
-                line = fastqfile.readline()[:-1]
+                pass
             else:
                 seq_counter += 1
                 if seq_counter in to_assess:
                     length = len(line[:-1])
                     random_length.append(length)
-                line = fastqfile.readline()[:-1]
+            line = fastqfile.readline()[:-1]
     len_mean = mean(random_length)
     len_stdev = int(stdev(random_length))
-    if len_mean < 150 and len_stdev > 1:
+    if len_mean < min_len and len_stdev > max_len_std:
         return False
     else:
         return True
 
 
 def preparation_main(dir_path):
-    fastqs_dir = Path(args.directory).resolve()
+    fastqs_dir = Path(dir_path)
     if fastqs_dir.is_file():
-        exit("Given path as directory is a file!!!")
+        exit("Given path as directory is actually a file!!!")
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M')
-    pickle_file = fastqs_dir / "{}.pk".format(timestamp)
-    fastqs = fastqs_dir.rglob("./**/*.fastq*")
+    pickle_file = Path(os.getcwd()) / "{}.pk".format(timestamp)
+    pks = fastqs_dir.rglob("./**/*.pk*")
     parents = OD()
-    for fq in fastqs:
+    for pkfile in pks:
         # print(fq)
-        p = fq.parent
+        try:
+            pkf_obj = task_pickle(pkfile)
+        except Exception as e:
+            continue
+        if pkf_obj.task_dict["status"]["download"] != pkf_obj.scode_d["Done"]:
+            continue
+        p = Path(pkf_obj.task_dict["args"]["input_dir"])
         files = parents.get(p, [])
-        files.append(fq.name)
-        files = sorted(files)
+        fqfiles = p.glob("*.fastq*")
+        if not fqfiles:
+            continue
+        fqfiles = sorted(fqfiles)
+        files.extend(fqfiles)
+        files = sorted(list(set(files)))
         if len(files) == 2:
             assert(re.search(forw_file_match, files[0])), "Incorrect order of forward and reverse files."
         parents[p] = files
@@ -78,35 +91,56 @@ def preparation_main(dir_path):
                 typ, enc = mimetypes.guess_type(str(full_path))
                 if enc == 'gzip':
                     os.system("gunzip -f {}".format(full_path))
-                if typ == "application/zip":
+                elif typ == "application/zip":
                     os.system("unzip {}".format(full_path))
+            fqfiles = p.glob("*.fastq")
+            fqfiles = sorted(list(set(fqfiles)))
+            parents[p] = fqfiles
 
     with ThreadPool(pool_size) as pool:
-        proper_length_dict = {p: proper_length(p.joinpath(fs[0].replace(".gz", ""))) for p, fs in parents.items()}
+        proper_length_dict = {p: proper_length(p.joinpath(fs[0])) for p, fs in parents.items()}
 
-    # print(proper_length_dict)
     for p, proper in proper_length_dict.items():
         if proper:
-            new_dict = OD()
-            paired = "Yes" if len(parents[p]) == 2 else "No"
-            new_dict["input_dir"] = str(p.resolve())
-            new_dict["paired"] = paired
-            new_dict["forward_file"] = parents[p][0].replace(".gz", "")
-            new_dict["reverse_file"] = parents[p][-1].replace(".gz", "") if paired == "Yes" else ''
-            new_dict["input_id"] = p.stem
-            new_dict["spike_amount"] = 6
-            processing_dicts.append(new_dict)
+            acc_ = p.stem
+            pkfile = p.joinpath("{}.pk".format(acc_))
+            pkf_obj = task_pickle(str(pkfile.resolve()))
+            if len(parents[p]) == 1 and pkf_obj.task_dict["args"]["paried"] == "Yes":
+                # TODO demultiplexing
+                print("Fastq file in {} need to get split!!!".format(p))
+                continue
+                # Get the updated fastq files names
+            if len(parents[p]) > 2:
+                print("More than two fastq files in {}!!!".format(p))
+                continue
+            pkf_obj.task_dict["args"]["forward_file"] = parents[p][0]
+            pkf_obj.task_dict["args"]["reverse_file"] = ''
+            if pkf_obj.task_dict["args"]["paried"] == "Yes":
+                pkf_obj.task_dict["args"]["reverse_file"] = parents[p][1]
+            pkf_obj.task_dict["status"]["run"] = pkf_obj.scode_d["Started"]
+            pkf_obj.write()
+            if pkf_obj.args_complete():
+                processing_dicts.append(pkf_obj.task_dict["args"])
+            else:
+                print("Incomplete task arguments in {}".format(p))
+
     with open(pickle_file, 'wb') as dest:
         pk.dump(processing_dicts, dest, protocol=pk.HIGHEST_PROTOCOL)
     return pickle_file
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory",
-                        help="The directory to find recursively all fastq filese inside",
+                        help="The directory to find recursively all process pk files inside",
                         default=".")
+    parser.add_argument("-nc", "--no-call",
+                        help="If call the tasks from pickle files.",
+                        default=False, action='store_true')
     args = parser.parse_args()
     fastqs_dir = Path(args.directory).resolve()
     pickle_file_path = preparation_main(fastqs_dir)
-    print(pickle_file_path)
-    process_multi(pickle_file_path)
+    if not args.no_call:
+        process_multi(pickle_file_path)
+    else:
+        print(pickle_file_path)
