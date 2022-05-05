3a225597cc84c41a91d938750a755b851b013b9e
Mohsen Pourjam <pourjam.cs@hotmail.com>
2022-05-05 16:23:56 +0200

    find_files updated

--- file_tools/find_files.py
+++ file_tools/find_files.py
@@ -130,7 +130,7 @@ def main(name_file, search_dir, zip_b=False, use_glob=False, use_walk=True, flat
                     # Splitting the fi into word list to prevent inclusion of small numbers in bigger numbers.
                     word_reg = re.compile(r"[-_\.\\ ]+")
                     fi_word_list = re.split(word_reg, fi.lower())
-                    if any([True for n in names_pats if str(n).lower() in fi_word_list]):
+                    if any([True for n in names_pats if str(n).lower() in fi.lower()]):
                         add_f = True
                 if add_f:
                     y_files.append(fi_path)
