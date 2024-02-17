0ab58017455686f5485b9f659c168d45d507fed1
Mohsen Pourjam <pourjam.cs@hotmail.com>
2024-02-17 13:23:59 +0000

    spike normalizer riases exception if row number of spike stat file and samples count in OTU Table is not equal

--- imngs2_pipeline/spike_normalizer.py
+++ imngs2_pipeline/spike_normalizer.py
@@ -4,7 +4,9 @@ import subprocess
 import math
 import statistics
 import pandas as pd
-from imngs2_pipeline.processing_helper import gimmelogger
+# from imngs2_pipeline.processing_helper import gimmelogger
+from processing_helper import gimmelogger
+
 from sys import version_info
 if version_info[0] < 3:
     from pathlib2 import Path  # pip2 install pathlib2
@@ -34,6 +36,8 @@ def normalize_otu_table(otu_table_path: str, spikes_stats_path: str, norm_method
     observed_weights = []
     observed_amounts = []
     observed_counts = []
+    sum = 0
+    n = 0
     with open(spikes_stats_path, 'r') as stats_h:
         for line in stats_h:
             if line.startswith("#"):
@@ -46,6 +50,7 @@ def normalize_otu_table(otu_table_path: str, spikes_stats_path: str, norm_method
                 try:
                     spike_reads = int(fields[1])
                 except Exception:
+                    NORM_LOG.warning(f"Could not parse spike reads for sample {sample_id}. Setting to 0.")
                     spike_reads = 0
                 try:
                     original_total_weight_in_g = float(fields[2])
@@ -55,31 +60,28 @@ def normalize_otu_table(otu_table_path: str, spikes_stats_path: str, norm_method
                     amount = float(fields[3])
                 except Exception:
                     amount = float("nan")
+                if spike_reads > 0:
+                    sum += spike_reads
+                    n += 1
+                    observed_counts.append(spike_reads)
                 if not (math.isclose(amount, 0, abs_tol=1e-5) or math.isnan(amount)):
                     observed_amounts.append(amount)
-                if not math.isclose(spike_reads, 0):
-                    observed_counts.append(spike_reads)
                 if not math.isnan(original_total_weight_in_g):
                     observed_weights.append(original_total_weight_in_g)
                 samples[sample_id] = (spike_reads, original_total_weight_in_g, amount)
 
-    sum = 0
-    n = 0
-    for values in samples.values():
-        count, _, amount = values
-        if not ((math.isclose(amount, 0, abs_tol=1e-5) or math.isnan(amount)) and math.isclose(count, 0, abs_tol=1e-5)):
-            sum += count
-            n += 1
-
     otu_table = pd.read_csv(otu_table_path, delimiter="\t", index_col=0)
 
-    if n == 0 or len(observed_amounts) == 0 or len(observed_counts) == 0:
+    if len(observed_amounts) == 0 or len(observed_counts) == 0:
         # delete "Spike" from norm_methods set if amount and count are not inferralbe from the rest of entries
-        NORM_LOG.warning(f"Normalization method 'Spike' is not applicable. No spike reads or amount found in spike stats file.")
+        NORM_LOG.warning(f"Normalization method 'Spike' is not applicable."
+                         " No valid spike reads or amount found in spike stats file.")
         norm_methods.discard("Spike")
-        mean = otu_table.iloc[:, :-1].sum().min()
-    else:
-        mean = (sum / n)
+    # Raise index error if not all otu_tables columns are in samples
+    # Do not include the first and last column in the check
+    if not all([col in samples for col in otu_table.columns[1:-1]]):
+        raise IndexError(f"Columns in OTU table {otu_table_path.name} are not in spike stats file {spikes_stats_path.name}")
+    factor = 1
     norm_methods_to_return = set()
     for norm_method in norm_methods:
         otu_table_copy = otu_table.copy()
@@ -88,22 +90,35 @@ def normalize_otu_table(otu_table_path: str, spikes_stats_path: str, norm_method
                 count, weight, amount = values
                 factor = None
                 if norm_method == "Spike":
-                    thismean = mean
+                    thismean = sum / n  # mean of spike reads
                     if math.isnan(weight):
                             if len(observed_weights) == 0:
                                 weight = 1  # fallback to 1
                             else:
                                 weight = statistics.median(observed_weights)  # fallback to median weight
                     # At this point we are sure that either observed_amounts or observed_counts are not empty
-                    if math.isclose(amount, 0, abs_tol=1e-5) or math.isnan(amount):
-                        amount = statistics.median(observed_amounts)
+                    # If amount is 0 then we exclude this sample from normalization
+                    # if amount is nan then we use the median of the observed amounts
                     if math.isclose(count, 0, abs_tol=1e-5):
                         count = statistics.mean(observed_counts)
+                    # TODO: correct it
+                    if math.isnan(amount):
+                        amount = statistics.median(observed_amounts)
+                    # If count is 0 then the value is treated as missed and we exclude this sample from normalization
+                    elif math.isclose(amount, 0, abs_tol=1e-5):
+                        # NOTE: What if we put amount to 0.00001 when it is 0?
+                        # dropping the column from otu_table_copy
+                        # set thismean to 1 to avoid division by 0 and having spike_count_factor = 1 
+                        count = thismean
+                        amount = 0.0001
+
                     molarity_factor = 600 / (amount * 100)
-                    factor = thismean / (count * weight * molarity_factor)
-                    normalized_otu_path = otu_table_path.parent.joinpath(f"SpikeNormalized-{str(otu_table_path.name)}")
+                    spike_count_factor = thismean / count
+                    # NOTE:TODO Having molarity factor and taking 600 Microliter of all samples
+                    # in the lab make normalization to weight meaningless
+                    factor = spike_count_factor / (weight * molarity_factor)
                 elif norm_method == "SampleMinCount":
-                    thismean = mean
+                    thismean = otu_table.iloc[:, :-1].sum().min()
                     factor = thismean / otu_table[file_id].sum()
                 elif norm_method.isnumeric():
                     factor = float(norm_method) / otu_table[file_id].sum()
