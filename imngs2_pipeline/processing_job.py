a02159f5c9e2416b9af2875f82433429a477a4fc
Mohsen Pourjam <pourjam.cs@hotmail.com>
2024-07-19 15:48:10 +0200

    keeping filtered2.fasta and derep.fasta in the preprocessing directory for de-novo analysis mode

--- imngs2_pipeline/processing_job.py
+++ imngs2_pipeline/processing_job.py
@@ -626,8 +626,8 @@ def cleanup(input_id, full_clean=False, dir_path=None):
         deleted_files = [
             'merged.fastq',  # output of merging paired reads step
             'filtered1.fastq',  # output of trimming step
-            'filtered2.fasta',  # output of amplicon error rate, length, basepair quality filtering step
-            'derep.fasta',  # output of dereplication of filtered2.fasta
+            # 'filtered2.fasta',  # output of amplicon error rate, length, basepair quality filtering step
+            # 'derep.fasta',  # output of dereplication of filtered2.fasta
             'sorted.fasta',  # output of sorting dereplicated sequences based on their sequence size
             'zotus.fasta',  # output of clustering sorted sequences to ZOTUs
             'good_ZOTUs.fa',  # output of filtering non 16S zotus
