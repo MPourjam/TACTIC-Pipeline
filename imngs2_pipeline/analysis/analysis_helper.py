ca08f6069f532f4c5d678d11a8d7f8d3d6b5911b
Mohsen Pourjam <pourjam.cs@hotmail.com>
2024-11-12 09:36:35 +0000

    temp file gets created in the same directory

--- imngs2_pipeline/analysis/analysis_helper.py
+++ imngs2_pipeline/analysis/analysis_helper.py
@@ -7,30 +7,31 @@ def onelinefasta(fastafilepath):
     proper_filepath = Path(fastafilepath).resolve()
     dirpath = proper_filepath.parent
     filename = proper_filepath.name
-    os.chdir(dirpath)
-    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".fasta")
+    temp_file = tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=str(proper_filepath.suffix),
+        dir=str(dirpath)
+    )
     newfile_temp_name = temp_file.name
     temp_file.close()
     with proper_filepath.open('r') as f, open(newfile_temp_name, "w+") as onelinefa:
         line = f.readline()
-        first = True
+        sequence = ""
         while line:
             if line[0] == '>':
-                if first:
-                    onelinefa.write(line)
-                    first = False
-                else:
-                    onelinefa.write('\n' + line)
-            elif line == '\n':
+                if sequence:
+                    onelinefa.write(sequence + "\n")
+                    sequence = ""
+                onelinefa.write(line)
+            elif line == '\n' or line == '\r\n':
                 pass
             else:
-                onelinefa.write(line[:-1])
+                sequence += line.strip()
             line = f.readline()
-        onelinefa.write("\n")
+        if sequence:
+            onelinefa.write(sequence + "\n")
     try:
-        # shutil.copy2(proper_filepath, proper_filepath.with_suffix(proper_filepath.suffix + ".bak"))  # for troubleshooting
         os.replace(newfile_temp_name, filename)
-        # print("{} sequence file is now oneline".format(filename))
     finally:
         if Path(newfile_temp_name).exists():
             Path(newfile_temp_name).unlink()
